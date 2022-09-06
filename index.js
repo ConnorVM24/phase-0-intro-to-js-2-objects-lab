185c702f67dffab04bc8916c7e36d509b08bd1b6
connorvm <connormannion24@yahoo.com>
2022-09-06 18:58:02 -0400

    commit tree 6703ac919c755ecd7c053ab151383aa93e3abaca

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {};
+employee.name = "Sweet Patot";
+employee.streetAddress = "11 Sweet Patot Rd";
+//employee.name = "isaac";
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copyOfemployee = {...employee};
+    copyOfemployee[key] = value;
+    return copyOfemployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee; 
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const copyOfemployee = {...employee};
+    delete copyOfemployee[key];
+    return copyOfemployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
